7cd82319ed20317a1be912d3e3f0446a29bf8dc8
leonardosartori-web <leonardo.sartori62@gmail.com>
2022-03-31 11:07:48 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+from myLib import *
+import pprint
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():  # put application's code here
+    if request.method == "POST":
+        url = request.form['url']
+        print(url)
+        data = getData(url)
+        pprint.pprint(data)
+        return redirect('/' + data['path'])
+    return render_template("index.html")
+
+
+@app.route('/<string:artist>', methods=["GET", "POST"])
+def artist(artist):
+    platforms = getPlatformObject(artist)
+    data = getData(platforms[0]["url"])
+    pprint.pp(data)
+    image = data["image"]
+    name = data["name"]
+    return render_template("page.html", platforms=platforms, image=image, name=name)
+
+
+@app.route('/<string:artist>/<string:track>', methods=["GET", "POST"])
+def track(artist, track):
+    platforms = getPlatformObject(artist + '/' + track)
+    data = getData(platforms[0]["url"])
+    pprint.pp(data)
+    social = getSocial(data)
+    image = data["image"]
+    name = data["name"]
+    return render_template("page.html", platforms=platforms, social=social, image=image, name=name)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ myLib.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import *
+from requests.structures import CaseInsensitiveDict
+import pprint
+import validators
+import tldextract
+
+def getData(link):
+    url = "https://songwhip.com/"
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+    data = "{\"url\":\"" + link + "\"}"
+    resp = requests.post(url, headers=headers, data=str(data))
+    return resp.json()
+
+
+def getLinksTrack(url):
+    xhtml = requests.get(url)
+    soup = BeautifulSoup(xhtml.text, "html.parser")
+    links = []
+    for link in soup.findAll('a'):
+        tmp = link.get('href')
+        if validators.url(tmp):
+            links.append(tmp)
+    links = links[:-4]
+    return links
+
+def refData(data):
+    for x in data:
+        pprint.pp(x)
+
+def getDomain(url):
+    res = tldextract.extract(url)
+    return res.domain.capitalize()
+
+def getPlatformObject(query):
+    links = getLinksTrack("https://songwhip.com/" + query)
+    platforms = []
+    for x in links:
+        platforms.append({"name": getDomain(x), "url": x})
+    return platforms
+
+
+def getSocial(data):
+    res = data["artists"][0]["links"]
+    keys = dict(res).keys()
+    platforms = ['facebook', 'instagram']
+    links = []
+    for x in platforms:
+        if x in keys:
+            tmp = {"url": res[x][0]["link"], "name": x}
+            links.append(tmp)
+    return links
+
+
+# pprint.pp(getSocial("https://open.spotify.com/album/5XpHo23Du4H9scYqGxLUIJ"))
+# pprint.pp(getData("https://open.spotify.com/artist/4NVhhX3tA4m84EqNNSOJV2"))
