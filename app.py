a263703a32dba8eedaaf9bf314d486f4a2a40664
leonardosartori-web <leonardo.sartori62@gmail.com>
2022-03-31 12:09:06 +0200

    Update

--- app.py
+++ app.py
@@ -16,48 +16,50 @@ def index():  # put application's code here
     return render_template("index.html")
 
 
-@app.route('/<string:artist>', methods=["GET", "POST"])
-def artist(artist):
+@app.route('/<string:artist>', methods=["GET"])
+def showArtist(artist):
+    print("Ciao")
+    print(artist)
     platforms = getPlatformObject(artist)
+    pprint.pp(platforms)
     data = getData(platforms[0]["url"])
-    pprint.pp(data)
     image = data["image"]
     name = data["name"]
     return render_template("page.html", platforms=platforms, image=image, name=name)
 
 
-@app.route('/<string:artist>/<string:track>', methods=["GET", "POST"])
+@app.route('/<string:artist>/<string:track>', methods=["GET"])
 def track(artist, track):
     platforms = getPlatformObject(artist + '/' + track)
     data = getData(platforms[0]["url"])
-    pprint.pp(data)
     social = getSocial(data)
     image = data["image"]
     name = data["name"]
     return render_template("page.html", platforms=platforms, social=social, image=image, name=name)
 
 
-@app.route('/api/<string:artist>', methods=["GET", "POST"])
+@app.route('/api/<string:artist>', methods=["GET"])
 def artistApi(artist):
     platforms = getPlatformObject(artist)
+    pprint.pp(platforms)
     data = getData(platforms[0]["url"])
-    pprint.pp(data)
     image = data["image"]
     name = data["name"]
     obj = {"name": name, "image": image, "platforms": platforms}
+    pprint.pp(obj)
     return obj
 
 
-@app.route('/api/<string:artist>/<string:track>', methods=["GET", "POST"])
+@app.route('/api/<string:artist>/<string:track>', methods=["GET"])
 def trackApi(artist, track):
     platforms = getPlatformObject(artist + '/' + track)
     data = getData(platforms[0]["url"])
-    pprint.pp(data)
     social = getSocial(data)
     image = data["image"]
     name = data["name"]
     obj = {"name": name, "image": image, "platforms": platforms, "social": social}
     return obj
+
 
 if __name__ == '__main__':
     app.run()
