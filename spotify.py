d1d70ce5bec0e560db680d8c7a8a48a9ec4e8d77
leonardosartori-web <leonardo.sartori62@gmail.com>
2022-04-06 21:33:30 +0200

    Update

--- spotify.py
+++ spotify.py
@@ -2,10 +2,12 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 
-cId = "3fffcfb44a32410aa1a7d89d343345cb"
-cs = "57860c20b80d47ecae4ea486b71db6ec"
-client_credentials_manager = SpotifyClientCredentials(client_id=cId, client_secret=cs)
+cid = '3fffcfb44a32410aa1a7d89d343345cb'
+secret = '57860c20b80d47ecae4ea486b71db6ec'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-artist = sp.user_follow_artists(["4NVhhX3tA4m84EqNNSOJV2"])
+artist = sp.artist("2ARH58Hit3yC6ziGdhma23")
+track = sp.track("70UbxfwWbeiglv4hbKcDz7")
+album = sp.album("5XpHo23Du4H9scYqGxLUIJ")
 pprint.pp(artist)
